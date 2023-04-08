8930871c265dfbfe71fe566b9e15c590b07528c9
Saurabh <saurabh4340@gmail.com>
2023-04-09 00:05:23 +0530

    saving cropped imgs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+import cv2
 from trocr import indic_trocr
 import utils
 
@@ -24,9 +26,13 @@ crops, location_preds = utils.prepare_crops_(
     
 )
 
-print(crops)
+# print(crops)
+for cropped_img in crops:
+    os.mkdir("cropped_imgs")
+    cv2.imread(os.path.join("cropped_imgs", cropped_img))
 # trocr prediction
 recognition_predictor = indic_trocr(model_path="trocr_files")
-word_preds = recognition_predictor([crop for page_crops in crops for crop in page_crops])
+word_preds = recognition_predictor(crops[0])
+# word_preds = recognition_predictor([crop for page_crops in crops for crop in page_crops])
 
 print(word_preds)
