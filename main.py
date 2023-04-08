bd2c1b04d1513b3bd9cb562bae890ca0f8a1dcdb
Saurabh <saurabh4340@gmail.com>
2023-04-09 00:12:04 +0530

    minor change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+import shutil
 from trocr import indic_trocr
 import utils
 
@@ -33,6 +34,9 @@ for cropped_img in crops:
 # trocr prediction
 recognition_predictor = indic_trocr(model_path="trocr_files")
 word_preds = recognition_predictor(crops[0])
+
+shutil.rmtree("cropped_imgs")
+
 # word_preds = recognition_predictor([crop for page_crops in crops for crop in page_crops])
 
 print(word_preds)
